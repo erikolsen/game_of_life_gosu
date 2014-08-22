753e84dd53dd77e24bc272d111fcdd93db0d6dc7
Erik Olsen <erik.s.olsen@gmail.com>
2014-08-21 19:43:47 -0500

    set up grid and cell

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,11 +1,18 @@
 
 
 class World
-  attr_accessor :rows, :cols
+  attr_accessor :rows, :cols, :grid
 
   def initialize(rows=100, cols=100)
     @rows = rows
     @cols = cols
+
+    @grid = Array.new(rows, Array.new(cols))
+
   end
 
+end
+
+class Cell
+  
 end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -6,15 +6,31 @@ describe 'Game of life' do
   context 'World' do
     subject { World.new }
     
-    it "should create a new world object" do
+    it 'should create a new world object' do
       expect(subject).to be_a(World)
     end
     
     it 'should respond to proper methods' do
       expect(subject).to respond_to(:rows)
       expect(subject).to respond_to(:cols)
+      expect(subject).to respond_to(:grid)
+    end
+
+    it 'should create proper grid on initialization' do
+      expect(subject.grid).to be_an Array
+      subject.grid.each do |row|
+        expect(row).to be_an(Array)
+      end
     end
 
 
+  end
+
+  context 'Cell' do
+    subject { Cell.new }
+
+    it 'should create a new cell' do
+      expect(subject).to be_a(Cell)
+    end
   end
 end
