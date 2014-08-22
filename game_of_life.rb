ce1a343b95bb278feb4c48b7f5838b7a2dc79f8f
Erik Olsen <erik.s.olsen@gmail.com>
2014-08-21 19:29:42 -0500

    can add rows and cols to world size

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,1 +1,11 @@
-#basic file
+
+
+class World
+  attr_accessor :rows, :cols
+
+  def initialize(rows=100, cols=100)
+    @rows = rows
+    @cols = cols
+  end
+
+end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -1,5 +1,20 @@
 require 'rspec'
+require_relative 'game_of_life.rb'
+
 
 describe 'Game of life' do 
-  
+  context 'World' do
+    subject { World.new }
+    
+    it "should create a new world object" do
+      expect(subject).to be_a(World)
+    end
+    
+    it 'should respond to proper methods' do
+      expect(subject).to respond_to(:rows)
+      expect(subject).to respond_to(:cols)
+    end
+
+
+  end
 end
