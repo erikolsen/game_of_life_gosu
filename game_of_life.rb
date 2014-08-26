eee0a7c7b482b58dea4ead2d1a27fe156333ecef
Erik Olsen <erik.s.olsen@gmail.com>
2014-08-25 20:30:44 -0500

    testing for rules

--- game_of_life.rb
+++ game_of_life.rb
@@ -10,21 +10,103 @@ class Game
     end
   end
 
-end
+  def tick!
+    next_itteration = check_rules
+    update_grid(next_itteration)
+  end
+
+  def update_grid(next_itteration)
+    world.cells.each_with_index do |cell, index| 
+      cell.alive = next_itteration[index]
+    end
+  end
+
+  def check_rules
+    next_itteration = []
+    world.cells.each do |cell|
+      n = world.live_neighbors_around_cell(cell).size
+      # rule 1
+      if cell.alive? && n < 2
+        next_itteration << false
+      #rule 2
+      elsif cell.alive? && (n == 2 || n == 3)
+        next_itteration << true
+      # rule 3
+      elsif cell.alive? && n > 3
+        next_itteration << false
+      #rule 4
+      elsif cell.dead? && n == 3
+        next_itteration << true
+      else
+        next_itteration << cell.alive?
+      end
+    end
+    return next_itteration
+  end
+
+end#game end
+
 
 class World
-  attr_accessor :rows, :cols, :grid
+  attr_accessor :rows, :cols, :grid, :cells
 
   def initialize(rows=3, cols=3)
     @rows = rows
     @cols = cols
-
+    @cells = []
     @grid = Array.new(rows) do |row|
               Array.new(cols) do |col|
-                Cell.new(col, row)
+                cell = Cell.new(col, row)
+                cells << cell
+                cell
               end
             end
+  end
 
+  def live_neighbors_around_cell(cell)
+    live_neighbors = []
+
+    # Neighbour to the North-East
+    if cell.y > 0 and cell.x < (cols - 1)
+      candidate = self.grid[cell.y - 1][cell.x + 1]
+      live_neighbors << candidate if candidate.alive?
+    end
+    # Neighbour to the South-East
+    if cell.y < (rows - 1) and cell.x < (cols - 1)
+      candidate = self.grid[cell.y + 1][cell.x + 1]
+      live_neighbors << candidate if candidate.alive?
+    end
+    # Neighbours to the South-West
+    if cell.y < (rows - 1) and cell.x > 0
+      candidate = self.grid[cell.y + 1][cell.x - 1]
+      live_neighbors << candidate if candidate.alive?
+    end
+    # Neighbours to the North-West
+    if cell.y > 0 and cell.x > 0
+      candidate = self.grid[cell.y - 1][cell.x - 1]
+      live_neighbors << candidate if candidate.alive?
+    end
+    # Neighbour to the North
+    if cell.y > 0
+      candidate = self.grid[cell.y - 1][cell.x]
+      live_neighbors << candidate if candidate.alive?
+    end
+    # Neighbour to the East
+    if cell.x < (cols - 1)
+      candidate = self.grid[cell.y][cell.x + 1]
+      live_neighbors << candidate if candidate.alive? 
+    end
+    # Neighbour to the South
+    if cell.y < (rows - 1)
+      candidate = self.grid[cell.y + 1][cell.x]
+      live_neighbors << candidate if candidate.alive?
+    end
+    # Neighbours to the West
+    if cell.x > 0
+      candidate = self.grid[cell.y][cell.x - 1]
+      live_neighbors << candidate if candidate.alive?
+    end
+    return live_neighbors
   end
 
 end
@@ -38,8 +120,16 @@ class Cell
     @y = y
   end
 
+  def die!
+    @alive = false
+  end
+
   def alive?
     alive
   end
+
+  def dead?
+    !alive
+  end 
 
 end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -1,7 +1,6 @@
 require 'rspec'
 require_relative 'game_of_life.rb'
 
-
 describe 'Game of life' do 
   
   let!(:world) { World.new }
@@ -17,6 +16,7 @@ describe 'Game of life' do
       expect(subject).to respond_to(:rows)
       expect(subject).to respond_to(:cols)
       expect(subject).to respond_to(:grid)
+      expect(subject).to respond_to(:live_neighbors_around_cell)
     end
 
     it 'should create proper grid on initialization' do
@@ -29,7 +29,67 @@ describe 'Game of life' do
       end
     end
 
+    it 'should add all cells to cells array' do 
+      expect(subject.cells.count).to eq(9)
+    end
 
+    # [[0,0],[0,1],[0,2],
+    #  [1,0],[1,1],[1,2],
+    #  [2,0],[2,1],[2,2]]
+
+    it 'should detect its northern neighbor' do
+      expect(subject.grid[0][1]).to be_dead
+      subject.grid[0][1].alive = true
+      expect(subject.grid[0][1]).to be_alive
+      expect(subject.live_neighbors_around_cell(subject.grid[1][1]).count).to eq(1)
+    end
+
+    it 'should detect its southern neighbor' do
+      subject.grid[1][1].alive = true
+      expect(subject.live_neighbors_around_cell(subject.grid[0][1]).count).to eq(1)
+    end
+
+    it 'should detect its eastern neighbor' do
+      subject.grid[1][1].alive = true
+      expect(subject.live_neighbors_around_cell(subject.grid[1][0]).count).to eq(1)
+    end
+    
+    it 'should detect its western neighbor' do
+      subject.grid[1][1].alive = true
+      expect(subject.live_neighbors_around_cell(subject.grid[1][2]).count).to eq(1)
+    end
+    
+    it 'should detect its north-western neighbor' do
+      subject.grid[1][1].alive = true
+      expect(subject.live_neighbors_around_cell(subject.grid[2][2]).count).to eq(1)
+    end
+
+    it 'should detect its north-eastern neighbor' do
+      subject.grid[1][1].alive = true
+      expect(subject.live_neighbors_around_cell(subject.grid[2][0]).count).to eq(1)
+    end
+    it 'should detect its south-eastern neighbor' do
+      subject.grid[1][1].alive = true
+      expect(subject.live_neighbors_around_cell(subject.grid[0][0]).count).to eq(1)
+    end  
+    
+    it 'should detect its south-western neighbor' do
+      subject.grid[1][1].alive = true
+      expect(subject.live_neighbors_around_cell(subject.grid[0][2]).count).to eq(1)
+    end
+
+    it 'should detect multiplie neighbors' do 
+      subject.grid[0][0].alive = true
+      subject.grid[0][1].alive = true
+      subject.grid[0][2].alive = true
+      subject.grid[1][0].alive = true
+      subject.grid[1][2].alive = true
+      subject.grid[2][0].alive = true
+      subject.grid[2][1].alive = true
+      subject.grid[2][2].alive = true
+      expect(subject.live_neighbors_around_cell(subject.grid[1][1]).count).to eq(8)
+
+    end    
   end
 
   context 'Cell' do
@@ -41,6 +101,7 @@ describe 'Game of life' do
 
     it 'should respond to proper methods' do
       expect(subject).to respond_to(:alive)
+      expect(subject).to respond_to(:die!)
       expect(subject).to respond_to(:x)
       expect(subject).to respond_to(:y)
     end
@@ -84,11 +145,35 @@ describe 'Game of life' do
       
       it 'should kill a live cell with 1 live neighbor' do
         game = Game.new(world, [[1,0],[2,0]])
-        expectgame.tick!
+        game.tick!
         expect(world.grid[1][0]).to be_dead
         expect(world.grid[2][0]).to be_dead
       end
 
+    end
+
+    context 'Rule 2 Any live cell with two or three live neighbors lives on to the next generation.' do
+      it 'should let cell live if it has two or three living neighbors' do 
+        game = Game.new(world,[[0,0],[0,1],[0,2]])
+        game.tick!
+        expect(world.grid[0][1]).to be_alive
+      end
+    end
+
+    context 'Rule 3 Any live cell with more than three live neighbors dies, as if by overcrowding.' do 
+      it 'should kill a live cell with more than three neighbors' do 
+        game = Game.new(world, [[0,0],[0,1],[0,2], [1,0], [1,1]])
+        game.tick!
+        expect(world.grid[1][1]).to be_dead
+      end
+    end
+
+    context 'Rule 4 Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.' do 
+      it 'should awaken a dead cell if it has exactly three live neighbors' do 
+        game = Game.new(world, [[0,0],[0,1],[0,2]])
+        game.tick!
+        expect(world.grid[1][1]).to be_alive
+      end
     end
 
   end
